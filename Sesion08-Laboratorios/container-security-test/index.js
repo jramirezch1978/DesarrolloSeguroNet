6671749852cc040f77bb4b8524642c8f91d815b0
Jhonny Ramirez <jramirez@npssac.com.pe>
2025-08-01 22:34:56 -0500

    cambios laboratorio 10

---
+++ Sesion08-Laboratorios/container-security-test/index.js
@@ -0,0 +1,4 @@
+﻿const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Security Test App'));
+app.listen(3000, () => console.log('App running on port 3000'));
